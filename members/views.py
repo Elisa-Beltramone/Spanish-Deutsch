674153e9a7a00c68b22c935553d0cd88436d2a82
Elisa <elibeltramone@gmail.com>
2024-08-05 11:43:52 +0200

    update

--- members/views.py
+++ members/views.py
@@ -4,12 +4,23 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.success(request, ("There was an error."))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
 
 def logout_user(request):
     logout(request)
-    messages.success(request, ("You logged out"))
-    return redirect('index')
+    messages.success(request, ("You logged out."))
+    return redirect('login')
 
 # def register_user(request):
     # if request.method == "POST":
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from datetime import datetime
+from django.contrib.auth.models import User
+
 from .models import Game
 from .forms import GameForm
 
 def index(request):
     form = UserCreationForm()
     now = datetime.now().date
-    return render(request, 'index.html', {'form':form, 'now':now})
+    users = User.objects.all()
+    return render(request, 'index.html', {'form':form, 'now':now, 'users':users})
 
 def personal_data(request):
      all_games = Game.objects.all()
